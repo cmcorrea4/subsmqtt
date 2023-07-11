7619c9cd37a9475652d7984e1db10af5277edb21
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-11 18:20:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("your_topic")
+    client.subscribe("Sensores")
 
 def on_message(client, userdata, msg):
     st.write(msg.topic+" "+str(msg.payload))
@@ -17,7 +17,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("mqtt.eclipse.org", 1883, 60)
+    client.connect("157.230.214.127", 1883, 60)
 
     while True:
         client.loop_start()
